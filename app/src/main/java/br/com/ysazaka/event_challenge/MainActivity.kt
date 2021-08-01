8a16a6fa62ee3989f7e353c367f2293ab7d67447
Glauco Sazaka <ysazaka@gmail.com>
2021-08-01 12:15:40 -0300

    Initial commit

---
+++ app/src/main/java/br/com/ysazaka/event_challenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.ysazaka.event_challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
