0cfb59c4a389f7d2848745e5a8a30dcb95419d37
Glauco Sazaka <ysazaka@gmail.com>
2021-08-02 23:52:01 -0300

    Adicionado a ação de compartilhar o evento

--- app/src/main/java/br/com/ysazaka/event_challenge/ui/activity/EventDetailActivity.kt
+++ app/src/main/java/br/com/ysazaka/event_challenge/ui/activity/EventDetailActivity.kt
@@ -3,16 +3,13 @@ package br.com.ysazaka.event_challenge.ui.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import br.com.ysazaka.event_challenge.R
 import br.com.ysazaka.event_challenge.databinding.ActivityEventDetailBinding
 import br.com.ysazaka.event_challenge.dto.EventDto
 import br.com.ysazaka.event_challenge.ui.activity.base.BaseActivity
-import br.com.ysazaka.event_challenge.ui.adapter.EventListAdapter
 import br.com.ysazaka.event_challenge.util.extensions.getDate
 import br.com.ysazaka.event_challenge.viewmodel.GetSelectedEventViewModel
 import com.bumptech.glide.Glide
-import org.koin.android.ext.android.bind
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class EventDetailActivity : BaseActivity() {
@@ -26,21 +23,44 @@ class EventDetailActivity : BaseActivity() {
     private val eventId by lazy {
         intent.getStringExtra(EVENT_ID)?: "0"
     }
+    private lateinit var event: EventDto
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        setupBackButton()
+        setupToolbar()
         setupObservers()
 
         showLoading()
         viewModel.getEventList(eventId)
     }
 
-    private fun setupBackButton() {
+    private fun setupToolbar() {
         binding.toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        binding.toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.item_share -> {
+                    shareEvent()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun shareEvent() {
+        val eventShareIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT,
+                resources.getString(R.string.share_intent_description, event.title))
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(eventShareIntent, null)
+        startActivity(shareIntent)
     }
 
     private fun setupObservers() {
@@ -52,12 +72,14 @@ class EventDetailActivity : BaseActivity() {
     }
 
     private fun setupEvent(event: EventDto) {
+        this.event = event
         hideLoading()
 
         Glide.with(this)
             .load(event.image)
             .error(resources.getDrawable(R.drawable.ic_event))
             .into(binding.ivEventDetail)
+
         binding.tvEventName.text = event.title
         binding.tvEventDate.text = resources.getString(R.string.event_date, event.date.getDate())
         binding.tvEventDescription.text = event.description
